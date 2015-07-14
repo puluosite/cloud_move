df89ce6befab3aff4632f2e9af968a3226e5cc1e
puluosite <mynameisprost@gmail.com>
2015-07-13 23:45:46 -0700

    if multiple optima exist, look for the one that is closest to the center. What left undone is adding threshold

--- MAD.m
+++ MAD.m
@@ -5,10 +5,13 @@ function [ diff ] = MAD( seg, pixel_n, position, num)
 % position.segc
 % position.dx
 % position.dy
-x = (position.segr-1)*num+1 + position.dx;
-y = (position.segc-1)*num+1 + position.dy;
+orig_row = (position.segr-1)*num+1;
+orig_col = (position.segc-1)*num+1;
 
-com_seg = pixel_n((x:x+num-1),(y:y+num-1));
+comp_row = orig_row + position.dx;
+comp_col = orig_col + position.dy;
+
+com_seg = pixel_n((comp_row:comp_row+num-1),(comp_col:comp_col+num-1));
 
 real_seg = int16(seg);
 real_com_seg = int16(com_seg);
@@ -17,14 +20,16 @@ msqr = true;
 if (msqr == true)
     temp_matrix = abs(real_seg - real_com_seg);
     temp_matrix = temp_matrix.^2;
-    diff.value = sum(sum(temp_matrix));
+    % normalize the sum
+    % value = sum(pix_diff^2)/Num_pix
+    diff.value = sum(sum(temp_matrix))/(num*num);
     
 else
     diff.value = sum(sum(abs(real_seg - real_com_seg)));
 end
 
-diff.x = position.dx;
-diff.y = position.dy;
+diff.row = position.dx;
+diff.col = position.dy;
 
 end
 
